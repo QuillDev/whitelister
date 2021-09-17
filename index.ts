e26f1e3edd4598e45304e04bb4fa3201287fe37e
Robert Brunson <siinspam@gmail.com>
2021-09-17 18:55:55 -0400

    fixed

--- index.ts
+++ index.ts
@@ -19,24 +19,24 @@ const apiUrl = "https://playerdb.co/api/player/minecraft";
     let succeeded = 0;
 
     //Async iterate through the array and populate the data
-    const data: { username: string, uuid: string }[] = [];
+    const data: { uuid: string, name: string, }[] = [];
     await Promise.all(names.map(async (name) => {
         return get(`${apiUrl}/${name}`)
-        .then((res) => JSON.parse(res.body))
-        .then((body) => {
-            processed++;
-            console.log(`Processed: ${Math.round((processed / names.length) * 100)}%`)
+            .then((res) => JSON.parse(res.body))
+            .then((body) => {
+                processed++;
+                console.log(`Processed: ${Math.round((processed / names.length) * 100)}%`)
 
-            if (body.code != "player.found") {
-                console.log(`Could not find uuid for player ${name}!`);
-                return;
-            }
-            data.push({ username: name, uuid: body.data.player.id as string });
-            succeeded++;
-        }).catch(console.error);
+                if (body.code != "player.found") {
+                    console.log(`Could not find uuid for player ${name}!`);
+                    return;
+                }
+                data.push({ uuid: body.data.player.id as string, name: name });
+                succeeded++;
+            }).catch(console.error);
     }))
 
-    
+
     writeFile("whitelist.json", JSON.stringify(data, null, 2), "utf-8");
     console.log(`Wrote to whitelist.json, Succeeded on ${succeeded} / ${names.length}`);
 })()
